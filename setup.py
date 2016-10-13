90400c40a2b72f0b4835a60fca31b65ed69ea945
Fred Prieur <fprieur@gmail.com>
2016-10-13 18:17:49 -0400

    correction erreur au niveau du fichier d'installation de projet

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ setup(
     name='vdmcli',
     version='0.1',
     py_modules=['vdmcli'],
-    include_package_data=True,
     install_requires=[
         'click',
     ],
     entry_points='''
         [console_scripts]
-        vdmclio=vdmcli:cli
+        vdmcli=vdmcli:cli
     ''',
 )
