aaf578c5f7bd8d1cd45cd693bf92aeac0315821b
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-10-27 01:08:22 -0400

    ajout d'un test unitaires avec des mocking object

--- tests/units/test_repo_bitbucket.py
+++ tests/units/test_repo_bitbucket.py
@@ -5,11 +5,18 @@ if __name__ == '__main__':
 
     import click
     from click.testing import CliRunner
+    from mock import MagicMock
+    from repo_bitbucket import repo_bitbucket
 
     runner = CliRunner()
 
+    # tester la création d'un nouveau dépôt avec un nom correct sans nom de projet
     with test("create_repo_with_repo_name"):
-        result = runner.invoke(vdmcli.cli,
-                               ['repo_create', '--name', 'allotest'])
-        result.output.must_equal(u"success\n")
+        repo_create_mock = repo_bitbucket.Repo_bitbucket()
+        repo_create_mock.createRepo = MagicMock(return_value="success")
+        result = repo_create_mock.createRepo("nomdepot")
+        #result = runner.invoke(vdmcli.cli,
+        #                       ['repo_create', '--name', 'allotest'])
+        if (repo_create_mock.createRepo.assert_called_once_with("nomdepot")):
+            result.must_equal("success")
     print result.ppl()
