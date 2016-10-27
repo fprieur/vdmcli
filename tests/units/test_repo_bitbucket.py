e42320f5677989d9c2f5151ebf74f0f1ab8cd417
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-10-27 00:20:58 -0400

    ajout premier test avec minitest

--- tests/units/test_repo_bitbucket.py
+++ tests/units/test_repo_bitbucket.py
@@ -6,18 +6,10 @@ if __name__ == '__main__':
     import click
     from click.testing import CliRunner
 
-    @click.group()
-    def cli():
-        pass
+    runner = CliRunner()
 
-    @cli.command()
-    @click.option('--name', help='Repo name')
-    @click.option('--project', default="test", help='Project name')
-    def repo_create(name, project):
-        b = repo_bitbucket.Repo_bitbucket()
-
-        runner = CliRunner()
-        result = runner.invoke(cli, ['--name', 'allotest'])
-        assert result.exit_code == "success"
-        assert 'Debug mode is on' in result.output
-        assert 'Syncing' in result.output
+    with test("create_repo_with_repo_name"):
+        result = runner.invoke(vdmcli.cli,
+                               ['repo_create', '--name', 'allotest'])
+        result.output.must_equal(u"success\n")
+    print result.ppl()
