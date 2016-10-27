19649e38d6bfd87bb840fbae695b1fa183da7d09
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-10-27 13:11:53 -0400

    ajout des fonctions d'affaire de lecture de fichier yaml

--- readyaml/readyaml.py
+++ readyaml/readyaml.py
@@ -1,7 +1,42 @@
 import ruamel.yaml as yaml
 
-with open("vdm.yaml") as stream:
-    try:
-        print(yaml.load(stream))
-    except yaml.YAMLError as exc:
-        print(exc)
+
+class Readyaml(object):
+    def __init__(self):
+        self.file = "reponame"
+
+    def readUsers(self):
+        with open("./../vdm.yaml") as stream:
+            try:
+                fichier_yaml = yaml.load(stream)
+                users = fichier_yaml['users']
+                return users
+            except yaml.YAMLError as exc:
+                return ("fichier yaml manquant")
+
+    def readScm(self):
+        with open("./../vdm.yaml") as stream:
+            try:
+                fichier_yaml = yaml.load(stream)
+                scm = fichier_yaml['scm']
+                return scm
+            except yaml.YAMLError as exc:
+                return ("fichier yaml manquant")
+
+    def readTestUnitsDirectory(self):
+        with open("./../vdm.yaml") as stream:
+            try:
+                fichier_yaml = yaml.load(stream)
+                unitstestdir = fichier_yaml['tests'][0]['units']
+                return unitstestdir
+            except yaml.YAMLError as exc:
+                return ("fichier yaml manquant")
+
+    def readTestIntegrationDirectory(self):
+        with open("./../vdm.yaml") as stream:
+            try:
+                fichier_yaml = yaml.load(stream)
+                integrationtestdir = fichier_yaml['tests'][1]['integration']
+                return integrationtestdir
+            except yaml.YAMLError as exc:
+                return ("fichier yaml manquant")
