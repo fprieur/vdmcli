3e1d8a0c31a243533493479a0b43877001a302c9
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-10-27 19:54:58 -0400

    ajout structure modèles pour la classe éxécutant les tests

---
+++ vdm_runtest/vdm_runtest.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+
+class Vdm_runtest(object):
+    def __init__(self):
+        self.directory = "./tests"
+
+    def testsUnits(self):
+        #  to be implement
+        print "success" if true else "error"
+
+    def testsIntegration(self):
+        #  to be implement
+        print "success" if true else "error"
+
+    def testsLoad(self):
+        #  to be implement
+        print "success" if true else "error"
