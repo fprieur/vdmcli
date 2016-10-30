d554ec57e6bd4934b9e9302b8541e4d718d2363e
Fred Prieur <fprieur@gmail.com>
2016-10-30 17:07:16 -0400

    implementation regle affaire pour jenkins

--- vdm_jenkins/vdm_jenkins.py
+++ vdm_jenkins/vdm_jenkins.py
@@ -6,14 +6,29 @@ class Vdm_jenkins(object):
     def __init__(self):
         self.jenkins_url = "http://localhost:8080"
 
-    def createProject(self, project_name):
-        #  to be implement
-        print "success" if project_name else "error"
+    def createBuild(self, build_name):
+        try:
+            server = self.authJenkins()
+            server.create_job(build_name, jenkins.EMPTY_CONFIG_XML)
+            print "success"
+        except Exception, e:
+            print ("erreur : " + str(e))
 
-    def createBuild(self, project_name, build_name):
-        #  to be implement
-        print "success" if project_name else "error"
+    def createView(self, view_name):
+        try:
+            server = self.authJenkins()
+            server.create_view(view_name, jenkins.EMPTY_VIEW_CONFIG_XML)
+            print "success"
+        except Exception, e:
+            print ("erreur : " + str(e))
 
     def runBuild(self, build_name):
         #  to be implement
         print "success" if build_name else "error"
+
+
+    def authJenkins(self):
+        server = jenkins.Jenkins("http://localhost:8080",
+                                 username="fred",
+                                 password="fred")
+        return server
