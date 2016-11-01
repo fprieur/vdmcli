71e0f19ee944392457d498e3b63e97030a5ae3fa
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-11-01 16:26:52 -0400

    ajout d'un tests pour la lecture de fichier

--- tests/units/test_vdm_readfile_read_users.py
+++ tests/units/test_vdm_readfile_read_users.py
@@ -10,9 +10,16 @@ if __name__ == '__main__':
 
     print("\n--- début tests unitaires pour la lecture des usager dans le fichier ---\n")
 
-    # tester la création d'un nouveau dépôt avec nom
+    # tests un tableau d'usager avec une valeur
     with test("create_jenkins_build_with_correct_param"):
         vdm_readfile_read_users_mock = vdm_readfile.Readfile()
         vdm_readfile_read_users_mock.readUsers = MagicMock(return_value=["toto"])
         result = vdm_readfile_read_users_mock.readUsers()
         result.must_equal(["toto"])
+
+    # tests un tableau d'usager vide
+    with test("create_jenkins_build_with_correct_param"):
+        vdm_readfile_read_users_mock = vdm_readfile.Readfile()
+        vdm_readfile_read_users_mock.readUsers = MagicMock(return_value=[])
+        result = vdm_readfile_read_users_mock.readUsers()
+        result.must_equal([])
