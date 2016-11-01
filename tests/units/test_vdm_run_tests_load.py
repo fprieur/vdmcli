07c09ab9843245f967eb0725c3c3c716751d052d
Fred Prieur <frederic.prieur@ville.montreal.qc.ca>
2016-11-01 18:43:03 -0400

    ajout des tests pour le domaine des test de stress

--- tests/units/test_vdm_run_tests_load.py
+++ tests/units/test_vdm_run_tests_load.py
@@ -8,3 +8,20 @@ if __name__ == '__main__':
     import click
     from click.testing import CliRunner
     from mock import MagicMock
+    from vdm_runtest import vdm_runtest
+
+    print("\n--- début des tests unitaires pour lancer les tests de stress ---\n")
+
+    # simulation que la réponse est positive
+    with test("run_load_test"):
+        vdm_runtest_mock = vdm_runtest.Vdm_runtest()
+        vdm_runtest_mock.testsLoad = MagicMock(return_value="success")
+        result = vdm_runtest_mock.testsLoad()
+        result.must_equal("success")
+
+    # simulation que la réponse est négative
+    with test("run_integration_tests_without_dir"):
+        vdm_runtest_mock = vdm_runtest.Vdm_runtest()
+        vdm_runtest_mock.testsLoad = MagicMock(return_value="error")
+        result = vdm_runtest_mock.testsLoad()
+        result.must_equal("error")
